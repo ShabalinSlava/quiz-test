785b22a48982a4b7f044da3be9ae79746435528b
Shabalin Slava <svaznoy0309@gmail.com>
2020-08-03 12:51:57 +0800

    create buildQuiz function

---
+++ main.js
@@ -0,0 +1,47 @@
+const quizContainer = document.getElementById("quiz")
+const resultsContainer = document.getElementById("results")
+const submitButton = document.getElementById('submit')
+
+const myQuestions = [
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of the these is a JavaScript package manager?",
+        answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            a: "Angular",
+            b: "jQuery",
+            c: "RequireJS",
+            d: "Eslint"
+        },
+        correctAnswer: "d"
+    }
+]
+
+function buildQuiz() {
+
+}
+
+function showResults() {
+
+}
+// display quiz right away
+buildQuiz()
+
+// on submit, show results
+submitButton.addEventListener("click", showResults)
